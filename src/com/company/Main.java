8e41055951c47c5470a97251ae186d366fe1bcc2
VeryBarry <barry.a.daniels@gmail.com>
2016-10-11 11:20:08 -0400

    displays top level

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,42 @@
 package com.company;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
 
+    static HashMap<String, User> users = new HashMap<>();
+    static ArrayList<Message> messages = new ArrayList<>();
+
     public static void main(String[] args) {
+        users.put("Alice", new User("Alice", "pass"));
+        users.put("Bob", new User("Bob", "pass"));
+        users.put("Charlie", new User("Charlie", "pass"));
+
+        messages.add(new Message(0, -1, "Alice", "Hello Everybody"));
+        messages.add(new Message(1, -1, "Bob", "How's it going er'body"));
+        messages.add(new Message(2, 0, "Charlie", "Cool Thread, Alice"));
+        messages.add(new Message(3, 2, "Alice", "Thanks Charlie"));
+
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    HashMap m = new HashMap();
+                    ArrayList<Message> msgs = new ArrayList<>();
+                    for (Message message : messages) {
+                        if (message.replyId == -1) {
+                            msgs.add(message);
+                        }
+                    }
+                    m.put("messages", msgs);
+                    return new ModelAndView(m, "home.html");
+                },
+        new MustacheTemplateEngine()
+        );
+
     }
 }
---
+++ src/com/company/User.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by VeryBarry on 10/11/16.
+ */
+public class User {
+    String username;
+    String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
